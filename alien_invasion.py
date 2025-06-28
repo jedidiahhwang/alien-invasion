6b2a0c7f284b6249840db66332c93d66a3c522de
Jeddy Hwang <jeddyhwang@Jeddys-MacBook-Pro.local>
2025-06-28 11:06:14 -0600

    initialized project class

---
+++ alien_invasion.py
@@ -0,0 +1,15 @@
+import sys
+
+import pygame
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+
+    def run_game(self):
+        """Start the main loop for the game."""
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
