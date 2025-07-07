0b88159b3050ac7e3e98f1fad63615790129556a
Jeddy Hwang <jeddyhwang@Jeddys-MacBook-Pro.local>
2025-07-07 10:22:10 -0600

    made ship background transparent

--- ship.py
+++ ship.py
@@ -9,6 +9,11 @@ class Ship:
         
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
+        
+        # Set the background color of the image to be transparent
+        # If it's a different color, change this value to match
+        self.image.set_colorkey((230, 230, 230))  # Makes white background transparent
+        
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen.
