c1674e2ee01edf30188052e3eefece08e2aa9f3e
Jeddy Hwang <jeddyhwang@Jeddys-MacBook-Pro.local>
2025-07-07 10:11:01 -0600

    refactoring methods

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -15,27 +16,33 @@ class AlienInvasion:
         self.clock = pygame.time.Clock()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(self.settings.screen_width, self.settings.screen_height)
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
 
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
         # Set the background color.
-        self.bg_color = (230, 230, 230) # RGB values
+        self.ship.blitme() # Draws the ship on the screen.
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
+            self.clock.tick(60) # Make the loop run 60 times per second or 60 FPS.
 
-        # Redraw the screen during each pass through the loop.
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
-
-        # Make the most recently drawn screen visible.
+        self.ship.blitme()
         pygame.display.flip()
-        self.clock.tick(60) # Make the loop run 60 times per second or 60 FPS.
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
