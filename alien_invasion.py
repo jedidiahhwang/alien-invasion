278ec9d30a2dac3a44d72f967f4262fc317e7c15
Jedidiah Hwang <jedidiah.hwang.dev@gmail.com>
2025-07-22 14:41:27 -0600

    finished project with life counter functionality

--- alien_invasion.py
+++ alien_invasion.py
@@ -132,6 +132,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.sb.prep_score()
             self.sb.prep_level()
+            self.sb.prep_ships()
             self.game_active = True
 
             # Get rid of any remaining aliens and bullets.
@@ -200,6 +201,7 @@ class AlienInvasion:
         if self.stats.ships_left > 0:
             # Decrement ships_left.
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
             self.bullets.empty()
